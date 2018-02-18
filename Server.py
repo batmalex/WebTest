89a375089ae24689a5ac760fad5678dec4e73d4f
Alex B <batmalex@ya.ru>
2018-02-18 20:21:18 +0300

    Изменены настройки подключения  bindings

--- Server.py
+++ Server.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hi gues!"
+    return "Hi guys!"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80)
